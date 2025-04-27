bb2afe12cc16bea3d9feaa15bca460d8cacbfda8
fwhoami <clahfclns@gmail.com>
2025-04-27 16:14:44 +0300

    add Swagger documentation for API

--- osint-web-app/src/main/java/org/example/osintwebapp/controller/UserSearchController.java
+++ osint-web-app/src/main/java/org/example/osintwebapp/controller/UserSearchController.java
@@ -1,4 +1,26 @@
 package org.example.osintwebapp.controller;
 
+import java.util.Map;
+import org.example.osintwebapp.service.UserSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserSearchController {
+
+    private final UserSearchService userSearchService;
+
+    @Autowired
+    public UserSearchController(UserSearchService userSearchService) {
+        this.userSearchService = userSearchService;
+    }
+
+    @GetMapping("/search")
+    public Map<String, String> searchUserAccounts(@RequestParam String username) {
+        return userSearchService.searchUserAccounts(username);
+    }
 }
