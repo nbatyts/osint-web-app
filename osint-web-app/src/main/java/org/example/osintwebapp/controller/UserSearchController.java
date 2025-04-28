c5f3f71cc491c1628e6d60c8c1ee9358d472e35a
fwhoami <clahfclns@gmail.com>
2025-04-28 15:10:45 +0300

    Change searchUserAccounts endpoint from GET to POST

--- osint-web-app/src/main/java/org/example/osintwebapp/controller/UserSearchController.java
+++ osint-web-app/src/main/java/org/example/osintwebapp/controller/UserSearchController.java
@@ -1,11 +1,12 @@
 package org.example.osintwebapp.controller;
 
 import java.util.Map;
+import org.example.osintwebapp.dto.SearchRequest;
 import org.example.osintwebapp.service.UserSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +20,8 @@ public class UserSearchController {
         this.userSearchService = userSearchService;
     }
 
-    @GetMapping("/search")
-    public Map<String, String> searchUserAccounts(@RequestParam String username) {
-        return userSearchService.searchUserAccounts(username);
+    @PostMapping("/search")
+    public Map<String, String> searchUserAccounts(@RequestBody SearchRequest request) {
+        return userSearchService.searchUserAccounts(request.getUsername());
     }
 }
